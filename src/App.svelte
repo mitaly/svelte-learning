6ed6c0953fdc3e3f6159cc9063e2c1e1ae7ba378
mitaly <senmitaly13@gmail.com>
2023-07-10 21:16:04 +0530

    if, else if, else

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,38 @@
 <script>
-	const name="Mitaly";
-	const winAPrize = `<a href="#" onclick="alert('You have won a prize')">Win a prize</a>`;
-	const headingId= 'heading';
-	const disabled = true;
-	const greenClass = 'green';
-	const isPromoted = true;
-	const promoted = false;
+	// const name="Mitaly";
+	// const winAPrize = `<a href="#" onclick="alert('You have won a prize')">Win a prize</a>`;
+	// const headingId= 'heading';
+	// const disabled = true;
+	// const greenClass = 'green';
+	// const isPromoted = true;
+	// const promoted = false;
+
+	const num = '-1';
+	console.log("Num", typeof num, num > 0);
 </script>
 
 <main>
-	<h1 id={headingId}>Hello {name}!</h1>
+	{#if num === 0}
+		<h1>Num is 0</h1>
+	{:else if num > 0}
+		<h1>Num is positive</h1>
+	{:else if num < 0}
+		<h1>Num is negative</h1>
+	{:else}
+		<h1>Not a number</h1>
+	{/if}
+
+	<!-- <h1 id={headingId}>Hello {name}!</h1>
 	<h2 class={isPromoted ? 'promoted' : ''}>Movie title class with conditional binding</h2>
 	<h2 class:promoted={isPromoted}>Movie title class with directive conditional binding</h2>
 	<h2 class:promoted>Movie title class with shorthand directive conditional binding</h2>
 	<div>{@html winAPrize}</div>
 	<button class={greenClass} disabled={disabled}>Click me</button>
-	
+	 -->
 </main>
 
 <style>
-	.promoted {
+	/* .promoted {
 		font-style: italic;
 	}
 	main {
@@ -44,5 +57,5 @@
 
 	.green {
 		background-color: green;
-	}
+	} */
 </style>
